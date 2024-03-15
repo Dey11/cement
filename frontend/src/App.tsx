cd4a5fe7a18d5c8955a0db5f338eba8394daaa28
Dey11 <shreyandey11@gmail.com>
2024-03-15 22:42:20 +0530

    Add initial React components and configuration files

---
+++ frontend/src/App.tsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/home",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
