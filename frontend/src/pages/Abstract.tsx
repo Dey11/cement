c9de27a40a5a88a5ec6e9c4c14e35fd62d5d85dd
Dey11 <shreyandey11@gmail.com>
2024-03-16 04:17:10 +0530

    Add Abstract component to frontend

---
+++ frontend/src/pages/Abstract.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Abstract = () => {
+  return (
+    <div className=" mx-auto">
+      <h1 className="text-4xl font-semibold mb-5">Abstract</h1>
+      <p className=" leading-loose font-medium text-lg text-justify">
+        The cement industry plays a crucial role in the construction sector,
+        providing the primary building material for infrastructure and buildings
+        worldwide. This industry is very important in the field of construction
+        and building businesses. Ensuring consistent and high-quality cement is
+        essential for construction projects to meet safety and durability
+        standards. This machine learning model abstract outlines a novel
+        approach for establishing a Cement Quality Prediction system, leveraging
+        artificial intelligence and data analytics to enhance cement production
+        processes. In this project we also focuses on the fraud detection
+        approaches of cement production and dealership businesses.
+      </p>
+    </div>
+  );
+};
+
+export default Abstract;
