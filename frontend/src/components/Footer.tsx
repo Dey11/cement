f0729daa531eb6e48d4fe73c799bc2a73e5f42e0
Dey11 <shreyandey11@gmail.com>
2024-03-16 17:31:49 +0530

    Add Footer component and include it in RootLayout

---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className=" text-center rounded-t-sm ">
+      <hr className="border border-[#3c3d3d] " />
+      <div className="flex justify-between mx-3 text-sm font-semibold">
+        <div className="pt-2">Made by Dey</div>
+        <p className="pt-2">Predicting Cement Quality with Machine Learning</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const RootLayout = () => {
   return (
     <div className=" bg-[#1e1f1f] h-max min-h-screen text-[#d0d1d1]">
-      <div className=" p-4 max-w-[1340px] mx-auto">
+      <div className=" p-4 max-w-[1340px] mx-auto h-max min-h-screen flex flex-col">
         <Header />
-        {/* <div className="text-2xl font-bold">Logo</div> */}
-        <Outlet />
+        <div className="flex-1">
+          <Outlet />
+        </div>
+        <Footer />
       </div>
     </div>
   );
