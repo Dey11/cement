cfeab71c2926913a64d549c16a5b455fa00d4163
Dey11 <shreyandey11@gmail.com>
2024-03-16 17:46:33 +0530

    Update footer text in Footer component

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -5,8 +5,8 @@ const Footer = () => {
     <div className=" text-center rounded-t-sm ">
       <hr className="border border-[#3c3d3d] " />
       <div className="flex justify-between mx-3 text-sm font-semibold">
-        <div className="pt-2">Made by Dey</div>
         <p className="pt-2">Predicting Cement Quality with Machine Learning</p>
+        <div className="pt-2">Made by Dey</div>
       </div>
     </div>
   );
