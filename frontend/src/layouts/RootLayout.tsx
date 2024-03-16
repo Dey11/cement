fd1da6aa8f1d78ab0938a95f8a64d5da645b0038
Dey11 <shreyandey11@gmail.com>
2024-03-16 16:50:43 +0530

    Add Header component to RootLayout

--- frontend/src/layouts/RootLayout.tsx
+++ frontend/src/layouts/RootLayout.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Outlet } from "react-router-dom";
+import Header from "../components/Header";
 
 const RootLayout = () => {
-  return <div>RootLayout</div>;
+  return (
+    <div className=" bg-[#1e1f1f] h-max min-h-screen text-[#d0d1d1]">
+      <div className=" p-4 max-w-[1340px] mx-auto">
+        <Header />
+        {/* <div className="text-2xl font-bold">Logo</div> */}
+        <Outlet />
+      </div>
+    </div>
+  );
 };
 
 export default RootLayout;
