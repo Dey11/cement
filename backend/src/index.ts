de0080c18236b9406fe6ab200eca9cbe409c7c8f
Dey11 <shreyandey11@gmail.com>
2024-03-17 20:09:07 +0530

    Enable CORS in backend server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,12 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 dotenv.config();
-// import cors from "cors";
+import cors from "cors";
 import { connectToDb } from "./db";
 import router from "./routes/authRouter";
 
 const app: Express = express();
-// app.use(cors({ origin: "*" }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 connectToDb(process.env.URI!).then(() => {
